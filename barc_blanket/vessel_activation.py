fd78972c68eebd70c7473cc14dd5b06329e48c9f
ZanderKeith <zkeith@mit.edu>
2024-02-27 17:16:47 -0500

    Dropped the particles being used in the simulation

--- barc_blanket/vessel_activation.py
+++ barc_blanket/vessel_activation.py
@@ -118,7 +118,7 @@ def make_model():
         settings.photon_transport = False
         settings.source = source
         settings.batches = 5
-        settings.particles = int(1e5) # modify this to shorten simulation, default was 1e6 
+        settings.particles = int(1e4) # modify this to shorten simulation, default was 1e6 
         settings.statepoint = {'batches': [
             5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]}
         settings.output = {'tallies': True}
