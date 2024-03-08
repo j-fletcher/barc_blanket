4ccaa4795e4fb049eb3f70a4aafcd89142eddbe5
ZanderKeith <zkeith@mit.edu>
2024-03-08 11:00:42 -0500

    Refactored run_optimization.py to use the argparse module

--- run_optimization.py
+++ run_optimization.py
@@ -1,11 +1,45 @@
+"""Run a parameter sweep using Optuna.
+
+A sweep is defined in a 'sweep_config.yaml' file with the following structure:
+    ```
+    metric: metric name
+    direction: minimize or maximize
+    parameters:
+        parameter1:
+        distribution: int
+        max: 100
+        min: 0
+        parameter2:
+        distribution: float
+        log: true for log scale, false for linear scale
+        max: 1.0
+        min: 0.0
+        parameter3:
+        distribution: categorical
+        values:
+        - value1
+        - value2
+    ```
+
+"""
+
+
 import os
 import sys
 import yaml
 import optuna
+import argparse
 
 from barc_blanket.models.simple_geometry import make_model
 from barc_blanket.optimize_model import evaluate_metric
 from barc_blanket.utilities import working_directory
+
+def _parse_args():
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Run a parameter sweep using Optuna")
+    parser.add_argument("sweep_directory", type=str, help="Relative path to directory where all the sweep input and output files are stored.")
+    parser.add_argument("-n", "--num_trials", type=int, default=1, help="Number of trials to run. This will add num_trials to the existing trials in the sweep_results.db")
+    return parser.parse_args()
 
 def objective(trial, sweep_config):
     """ Objective function for the optimization
@@ -66,36 +100,12 @@ def objective(trial, sweep_config):
     
     return metric_val
 
-def main(sweep_directory, num_trials=1):
-    """Run a parameter sweep using Optuna
+def main():
+    # Parse command line arguments
+    args = _parse_args()
 
-    Parameters:
-    ----------
-    sweep_directory : str
-        Relative path to directory where all the sweep input and output files are stored.
-        MUST include a 'sweep_config.yaml' with the following structure:
-        ```
-        metric: metric name
-        direction: minimize or maximize
-        parameters:
-          parameter1:
-            distribution: int
-            max: 100
-            min: 0
-          parameter2:
-            distribution: float
-            log: true for log scale, false for linear scale
-            max: 1.0
-            min: 0.0
-          parameter3:
-            distribution: categorical
-            values:
-            - value1
-            - value2
-        ```
-    num_trials:int, default=1
-        Number of trials to run. This will add num_trials to the existing trials in the sweep_results.db
-    """
+    sweep_directory = args.sweep_directory
+    num_trials = args.num_trials
 
     # Change to the sweep directory
     with working_directory(sweep_directory):
@@ -119,12 +129,5 @@ def main(sweep_directory, num_trials=1):
 
         study.optimize(lambda trial: objective(trial, sweep_config), n_trials=num_trials)
 
-
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        main(sys.argv[1])
-    elif len(sys.argv) == 3:
-        main(sys.argv[1], int(sys.argv[2]))
-    else:
-        print("Usage: python run_optimization.py <relative_sweep_directory> <num_trials>")
-        sys.exit(1)
+    main()
