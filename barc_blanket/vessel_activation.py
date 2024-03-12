516686ae5c6c18ac3317187f81b5fbdad9ea87d4
ZanderKeith <zkeith@mit.edu>
2024-03-12 16:52:52 -0400

    Put cross sections and chain file path in utilities

--- barc_blanket/vessel_activation.py
+++ barc_blanket/vessel_activation.py
@@ -4,16 +4,12 @@ import openmc.deplete
 import numpy as np
 
 import os
-import pickle
 
-CROSS_SECTIONS = '/usr/local/share/xs_data/endfb80_hdf5/cross_sections.xml'
-CHAIN_FILE = '/home/zkeith/openmc_resources/chain_endfb80_sfr.xml'
+from barc_blanket.utilities import CROSS_SECTIONS, CHAIN_FILE
 
 # Heavily based on John's stuff here: https://github.com/jlball/arc-nonproliferation/tree/master/openmc-scripts/arc-1/independent_depletion
 
-
-
-def run_independent_vessel_activation(model:openmc.Model, days=365, num_timesteps=50, source_rate=2e20):
+def run_independent_vessel_activation(model:openmc.Model, days=365, num_timesteps=50, source_rate=3.6e20):
     """ Run the vessel activation after a certain number of days.
 
     Parameters:
@@ -25,7 +21,7 @@ def run_independent_vessel_activation(model:openmc.Model, days=365, num_timestep
     num_timesteps : int
         The number of timesteps to run the model for.
     source_rate : float
-        The source rate of neutrons in the model. Default is 2e20 (for a 500MW fusion reactor)
+        The source rate of neutrons in the model. Default is 3.6e20 (for 1 GW fusion power)
     """
 
     openmc.config['cross_sections'] = CROSS_SECTIONS
