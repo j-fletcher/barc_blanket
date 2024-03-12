f5b7c113e0e4e2e3515f8694e66b57f9f5fc0e56
ZanderKeith <zkeith@mit.edu>
2024-03-12 14:14:54 -0400

    Refactored simple_geometry.py to utilize new material imports and included a first wall material and burner vessel material

--- barc_blanket/models/simple_geometry.py
+++ barc_blanket/models/simple_geometry.py
@@ -15,6 +15,7 @@ DEFAULT_PARAMETERS = {
     'vv_thickness': 2,           # How thick the wall of the vacuum vessel is
     'fusion_blanket_width': 20,  # Width of the material in the fusion blanket
     'burner_blanket_width': 100, # Width of the material in the burner blanket
+    'bv_thickness': 5,           # How thick the burner vessel is
     'li6_enrichment': 0.076,     # atom% enrichment of Li6 in the FLiBe
     'slurry_ratio': 0.01         # wt% slurry in the burner blanket
 }
@@ -49,8 +50,10 @@ def make_model(new_model_config=None):
     plasma_material = dt_plasma
     first_wall_material = tungsten
     vv_material = v4cr4ti
-    fusion_material = enriched_flibe(model_config['li6_enrichment'])
-    burner_material = burner_mixture(model_config['slurry_ratio'])
+    fusion_blanket_material = enriched_flibe(model_config['li6_enrichment'])
+    burner_blanket_material = burner_mixture(model_config['slurry_ratio'])
+    bv_material = v4cr4ti
+
     
     #####################
     ## Define Geometry ##
@@ -59,13 +62,18 @@ def make_model(new_model_config=None):
     R = model_config['major_radius']
     a = model_config['plasma_minor_radius']
     sol_width = model_config['sol_width']
+    first_wall_thickness = model_config['first_wall_thickness']
     vv_thickness = model_config['vv_thickness']
     fusion_blanket_width = model_config['fusion_blanket_width']
     burner_blanket_width = model_config['burner_blanket_width']
+    bv_thickness = model_config['bv_thickness']
 
     plasma_surface = openmc.ZTorus(x0=0,y0=0,z0=0,a=R,b=a,c=a)
+    
+    first_wall_inner_radius = a + sol_width
+    first_wall_inner_surface = openmc.ZTorus(x0=0,y0=0,z0=0,a=R,b=first_wall_inner_radius,c=first_wall_inner_radius)
 
-    inboard_vv_inner_radius = a + sol_width
+    inboard_vv_inner_radius = first_wall_inner_radius + first_wall_thickness
     inboard_vv_outer_radius = inboard_vv_inner_radius + vv_thickness
     inboard_vv_inner_surface = openmc.ZTorus(x0=0,y0=0,z0=0,a=R,b=inboard_vv_inner_radius,c=inboard_vv_inner_radius)
     inboard_vv_outer_surface = openmc.ZTorus(x0=0,y0=0,z0=0,a=R,b=inboard_vv_outer_radius,c=inboard_vv_outer_radius)
@@ -75,51 +83,64 @@ def make_model(new_model_config=None):
     outboard_vv_inner_surface = openmc.ZTorus(x0=0,y0=0,z0=0,a=R,b=outboard_vv_inner_radius,c=outboard_vv_inner_radius)
     outboard_vv_outer_surface = openmc.ZTorus(x0=0,y0=0,z0=0,a=R,b=outboard_vv_outer_radius,c=outboard_vv_outer_radius)
 
-    burner_blanket_tank_inner_radius = outboard_vv_outer_radius+burner_blanket_width
-    burner_blanket_tank_surface = openmc.ZTorus(x0=0,y0=0,z0=0,a=R,b=burner_blanket_tank_inner_radius,c=burner_blanket_tank_inner_radius)
+    bv_inner_radius = outboard_vv_outer_radius+burner_blanket_width
+    bv_outer_radius = bv_inner_radius+bv_thickness
+    bv_inner_surface = openmc.ZTorus(x0=0,y0=0,z0=0,a=R,b=bv_inner_radius,c=bv_inner_radius)
+    bv_outer_surface = openmc.ZTorus(x0=0,y0=0,z0=0,a=R,b=bv_outer_radius,c=bv_outer_radius)
 
     bounding_sphere_surface = openmc.Sphere(r=2*R, boundary_type="vacuum")
 
     plasma_cell = openmc.Cell(
         name='plasma_cell',
         region=-plasma_surface,
-        fill=dt_plasma
+        fill=plasma_material
     )
 
     sol_cell = openmc.Cell(
         name='sol_cell',
-        region=+plasma_surface & -inboard_vv_inner_surface,
+        region=+plasma_surface & -first_wall_inner_surface,
         fill=None
+    )
+
+    first_wall_cell = openmc.Cell(
+        name='first_wall_cell',
+        region=+first_wall_inner_surface & -inboard_vv_inner_surface,
+        fill=first_wall_material
     )
 
     inboard_vv_cell = openmc.Cell(
         name='inboard_vv_cell',
         region=+inboard_vv_inner_surface & -inboard_vv_outer_surface,
-        fill=v4cr4ti
+        fill=vv_material
     )
 
     fusion_blanket_cell = openmc.Cell(
         name='fusion_blanket_cell',
         region=+inboard_vv_outer_surface & -outboard_vv_inner_surface,
-        fill=flibe
+        fill=fusion_blanket_material
     )
 
     outboard_vv_cell = openmc.Cell(
         name='outboard_vv_cell',
         region=+outboard_vv_inner_surface & -outboard_vv_outer_surface,
-        fill=v4cr4ti
+        fill=vv_material
     )
 
-    # TODO: include slurry mixture
     burner_blanket_cell = openmc.Cell(
         name='burner_blanket_cell',
-        region=+outboard_vv_outer_surface & -burner_blanket_tank_surface,
-        fill=flibe
+        region=+outboard_vv_outer_surface & -bv_inner_surface,
+        fill=burner_blanket_material
+    )
+
+    bv_cell = openmc.Cell(
+        name='bv_cell',
+        region=+bv_inner_surface & -bv_outer_surface,
+        fill=bv_material
     )
 
     bounding_sphere_cell = openmc.Cell(
         name='bounding_sphere_cell',
-        region=+burner_blanket_tank_surface & -bounding_sphere_surface,
+        region=+bv_outer_surface & -bounding_sphere_surface,
         fill=None
     )
 
