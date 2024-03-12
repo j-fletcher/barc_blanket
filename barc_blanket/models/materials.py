4c1c8419b40762226b0d92a7b40e822dd04b8197
ZanderKeith <zkeith@mit.edu>
2024-03-12 15:23:03 -0400

    Made materials depletable

--- barc_blanket/models/materials.py
+++ barc_blanket/models/materials.py
@@ -8,23 +8,27 @@ dt_plasma.set_density('g/cm3', 1e-5)
 
 # FLIBE
 flibe = openmc.Material(name="flibe")
+flibe.depletable=True
 flibe.add_element("Li", 2.0, "ao")
 flibe.add_element("Be", 1.0, "ao")
 flibe.add_element("F", 4.0, "ao")
 flibe.set_density("g/cm3", 1.94)
 
 def enriched_flibe(li6_enrichment):
-    flibe = openmc.Material(name="enriched_flibe")
-    flibe.add_element("Li", 2.0, "ao", 
+    enriched_flibe = openmc.Material(name="enriched_flibe")
+    enriched_flibe.depletable=True
+    enriched_flibe.add_element("Li", 2.0, "ao", 
                         enrichment=li6_enrichment, 
                         enrichment_target="Li6", 
                         enrichment_type="ao")
-    flibe.add_element("Be", 1.0, "ao")
-    flibe.add_element("F", 4.0, "ao")
-    flibe.set_density("g/cm3", 1.94)
+    enriched_flibe.add_element("Be", 1.0, "ao")
+    enriched_flibe.add_element("F", 4.0, "ao")
+    enriched_flibe.set_density("g/cm3", 1.94)
+    return enriched_flibe
 
 # Inconel 718 -
 inconel718 = openmc.Material(name='inconel718')
+inconel718.depletable = True
 inconel718.add_element('Ni', 53.0, 'wo')
 inconel718.add_element('Cr', 19.06, 'wo')
 inconel718.add_element('Nb', 5.08, 'wo')
@@ -39,6 +43,7 @@ inconel718.set_density('g/cm3', 8.19)
 
 # Eurofer
 eurofer = openmc.Material(name='eurofer')
+eurofer.depletable = True
 eurofer.add_element('Cr', 8.99866, 'wo')
 eurofer.add_element('C', 0.109997, 'wo')
 eurofer.add_element('W', 1.5, 'wo')
@@ -56,6 +61,7 @@ eurofer.set_density('g/cm3', 7.798)
 
 # V-4Cr-4Ti - pure -(from Segantin TRE https://github.com/SteSeg/tokamak_radiation_environment)
 v4cr4ti = openmc.Material(name='v4cr4ti')
+v4cr4ti.depletable = True
 v4cr4ti.add_element('V', 0.92, 'wo')
 v4cr4ti.add_element('Cr', 0.04, 'wo')
 v4cr4ti.add_element('Ti', 0.04, 'wo')
@@ -63,18 +69,21 @@ v4cr4ti.set_density('g/cm3', 6.06)
 
 # Tungsten - pure
 tungsten = openmc.Material(name='tungsten')
+tungsten.depletable = True
 tungsten.add_element('W', 1.0, 'wo')
 tungsten.set_density('g/cm3', 19.3)
 
 # Water
 water = openmc.Material(name='water')
+water.depletable = True
 water.add_nuclide('H1', 2.0)
 water.add_nuclide('O16', 1.0)
 water.set_density('g/cm3', 1.0)
 
 # Raw tank contents, do however you want to define this
-tank_contents = openmc.Material(name='tank_contents')
 # For now just say it's natural uranium TODO can change this to be whatever, or make a function for it
+tank_contents = openmc.Material(name='tank_contents')
+tank_contents.depletable = True
 tank_contents.add_nuclide('U235', 0.0072)
 tank_contents.add_nuclide('U238', 0.9928)
 tank_contents.set_density('g/cm3', 19.1)
@@ -90,5 +99,6 @@ def burner_mixture(slurry_ratio):
         'ao',
         name="burner_mixture"
     )
+    burner_mixture.depletable = True
 
     return burner_mixture
