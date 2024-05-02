fixed unit conversion error

barc_blanket/models/barc_model_simple_toroidal.py
@@ -28,7 +28,9 @@ DEFAULT_PARAMETERS = {
     'removed_Sr90': 0.0,            # fraction of Sr90 removed from the waste stream
     'removed_Cs137': 0.0,           # fraction of Cs137 removed from the waste stream
     'removed_Tc99': 0.0,            # fraction of Tc99 removed from the waste stream
-    'removed_C0': 1.0,              # Remove elemental carbon from waste for depletion chain reasons
+    'removed_C0': 0.0,              # Remove elemental carbon from waste for depletion chain reasons
+    'removed_U': 0.0,              # Remove elemental carbon from waste for depletion chain reasons
+    'removed_Pu': 0.0,              # Remove elemental carbon from waste for depletion chain reasons
   
     'batches': 50,               # Number of batches to run
     'particles': int(1e5),       # Number of particles per batch
@@ -84,6 +86,12 @@ def make_model(new_model_config=None):
         removed_materials_dict['Tc99'] = model_config['removed_Tc99']
     if not np.isclose(model_config['removed_C0'], 0):
         removed_materials_dict['C0'] = model_config['removed_C0']
+    if not np.isclose(model_config['removed_U'], 0):
+        removed_materials_dict['U235'] = model_config['removed_U']
+        removed_materials_dict['U238'] = model_config['removed_U']
+    if not np.isclose(model_config['removed_Pu'], 0):
+        removed_materials_dict['Pu239'] = model_config['removed_Pu']
+        removed_materials_dict['Pu240'] = model_config['removed_Pu']
 
     if len(removed_materials_dict.keys()) > 0:
         cooling_channel_material = separate_nuclides(cooling_channel_material, removed_materials_dict)

run_coupled_depletion.py
@@ -10,8 +10,12 @@ openmc.config['chain_file'] = CHAIN_FILE
 
 # create model from barc_model_simple_toroidal.py
 
-model = make_model(new_model_config={'particles':200,
-                   'batches': 20})
+model = make_model(new_model_config={
+    'slurry_ratio': 0.05,
+    'removed_U': 0.0,
+    'removed_Pu': 0.0,
+    'particles': int(1e3)
+    })
 
 # Set timesteps and source rates
 # must have one source rate per timestep
@@ -24,8 +28,8 @@ print(sum(timesteps_years))
 
 efus = 17.6e6  # eV
 ev2j = 1.60218e-19
-Pfus = 2000e6  # W
-neutron_rate = Pfus / (efus / ev2j)  # n/s
+Pfus = 2200e2  # W
+neutron_rate = Pfus / (efus * ev2j)  # n/s
 
 source_rates = np.ones_like(timesteps)*neutron_rate
 
@@ -33,12 +37,12 @@ source_rates = np.ones_like(timesteps)*neutron_rate
 
 op = openmc.deplete.CoupledOperator(model, 
                                     reduce_chain=True, 
-                                    reduce_chain_level=5, 
+                                    reduce_chain_level=3, 
                                     normalization_mode='source-rate')
 
 # Set output directory 
 
-output_dir = Path('./depletion_results/endf8_chain_modified')
+output_dir = Path('./depletion_results/barc-v3-5-percent-waste')
 op.output_dir=output_dir
 
 # Run model