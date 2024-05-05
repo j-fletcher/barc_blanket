0dccb5415d5ace9f3673dcc3b213353f405f65d2
josephb-hall <hallefkt@mit.edu>
2024-05-05 11:19:39 -0400

    testing full tank inventory, loading from xml

--- barc_blanket/models/materials_tools.py
+++ barc_blanket/models/materials_tools.py
@@ -1,4 +1,5 @@
 import openmc
+import xml.etree.ElementTree as ET
 from .create_waste import create_waste_material
 
 # Plasma
@@ -92,43 +93,104 @@ def water():
     water.set_density('g/cm3', 1.0)
     return water
 
+def import_material_from_xml(materials_xml):
+    """
+    Create an openmc material by parsing an XML Materials file
+    Maybe there's a better way of doing this
+    """
+    # Parse the materials.xml file
+    tree = ET.parse(materials_xml)
+    root = tree.getroot()
+    print(root)
+    # Create an empty dictionary to store materials
+    materials = {}
+
+    # Loop through each <material> tag in the XML file
+    for material_elem in root.findall('material'):
+        # Get material ID and name
+        material_id = int(material_elem.get('id'))
+        print(material_id)
+        name = str(material_elem.get('name'))
+        print(name)
+
+        # Create a new OpenMC material object
+        material = openmc.Material(material_id, name)
+
+        # Loop through each <nuclide> tag within the material
+        for nuclide_elem in material_elem.findall('nuclide'):
+            nuclide = nuclide_elem.get('name')
+            atom_percent = float(nuclide_elem.get('ao'))
+
+            # Add the nuclide to the material with the specified atom percent
+            material.add_nuclide(nuclide, atom_percent)
+
+        # Store the material in the dictionary
+        materials[str(material_id)] = material
+
+    return materials
+
 # Raw tank contents, do however you want to define this
-def tank_contents():
-    tank1_SCS = create_waste_material('241-A-106','Saltcake Solid','241-A-106-SCS')
-    tank1_SLS = create_waste_material('241-A-106','Sludge (Liquid & Solid)','241-A-106-SLS')
-    tank1_SS = create_waste_material('241-A-106','Sludge Solid','241-A-106-SS')
+def get_tank_contents(id=1):
+    """
+    ID information
+    1 - most active tank, all waste phases present
+    2 - All-tank mix, all waste phases present
+    3 - All-tank mix, U, Th, Pu removed
+    4 - All-tank mix, sludge phases only, Sr, Cs removed
+    5 - All-tank mix, sludge phases only, Sr, Cs, U, Th, Pu removed
+    """
+    if id == 1:
+        # old workflow to get the most active tank material
+        tank1_SCS = create_waste_material('241-A-106','Saltcake Solid','241-A-106-SCS')
+        tank1_SLS = create_waste_material('241-A-106','Sludge (Liquid & Solid)','241-A-106-SLS')
+        tank1_SS = create_waste_material('241-A-106','Sludge Solid','241-A-106-SS')
 
-    #Tank Mixture waste phase volumes for 241-A-106 in L
-    #SIL_vol = 198000
-    SCS_vol = 84000
-    SLS_vol = 38000+110000
-    SS_vol = 41000
+        #Tank Mixture waste phase volumes for 241-A-106 in L
+        #SIL_vol = 198000
+        SCS_vol = 84000
+        SLS_vol = 38000+110000
+        SS_vol = 41000
 
-    total_vol = SCS_vol + SLS_vol + SS_vol
+        total_vol = SCS_vol + SLS_vol + SS_vol
 
-    #Waste Phase Volume Fraction
-    #SIL_frac = SIL_vol/total_vol
-    SCS_frac = SCS_vol/total_vol
-    SLS_frac = SLS_vol/total_vol
-    SS_frac = SS_vol/total_vol
+        #Waste Phase Volume Fraction
+        #SIL_frac = SIL_vol/total_vol
+        SCS_frac = SCS_vol/total_vol
+        SLS_frac = SLS_vol/total_vol
+        SS_frac = SS_vol/total_vol
 
-    #Tank material object containing all present waste phases
-    tank_contents = openmc.Material.mix_materials([tank1_SCS,tank1_SLS,tank1_SS],[SCS_frac,SLS_frac,SS_frac],'vo')
+        #Tank material object containing all present waste phases
+        tank_contents = openmc.Material.mix_materials([tank1_SCS,tank1_SLS,tank1_SS],[SCS_frac,SLS_frac,SS_frac],'vo')
+
+    elif id == 2:
+        material = import_material_from_xml("barc_blanket/materials/full_tank_inventory.xml")
+        tank_contents = material["1371"]
+    elif id == 3:
+        material = import_material_from_xml("barc_blanket/materials/full_tank_inventory_no_PuThU.xml")
+        tank_contents = material["1533"]
+    elif id == 4:
+        material = import_material_from_xml("barc_blanket/materials/sludge_plus_radionuclides_no_CsSr.xml")
+        tank_contents = material["1059"]
+    elif id == 5:
+        material = import_material_from_xml("barc_blanket/materials/sludge_plus_radionuclides_no_CsSrPuThU.xml")
+        tank_contents = material["1215"]
+    else:
+        raise ValueError("Invalid ID for tank contents. See tank_contents function docstring for valid IDs.")
 
     tank_contents.depletable = True
     tank_contents.name = 'tank_contents'
     return tank_contents
 
 # Mixture of tank contents and flibe for the blanket
-def burner_mixture(slurry_ratio, tank_contents=tank_contents(), flibe=flibe()):
+def burner_mixture(slurry_ratio, id=1, flibe=flibe()):
     """Create a mixture of flibe and tank contents for the blanket
     
     Parameters:
     ----------
     slurry_ratio : float
         The weight percent of slurry in the blanket
-    tank_contents : openmc.Material, optional
-        The tank contents to use in the mixture. Default is natural uranium.
+    id : int
+        Material ID to pass to the blanket mixture from tank_contents()
     flibe : openmc.Material, optional
         The FLiBe material to use in the mixture. Default is the standard FLiBe material.
         Can pass in enriched flibe if desired
@@ -139,6 +201,8 @@ def burner_mixture(slurry_ratio, tank_contents=tank_contents(), flibe=flibe()):
         The mixture of FLiBe and tank contents
     
     """
+    tank_contents=get_tank_contents(id)
+
     flibe_ao = 1 - slurry_ratio
 
     burner_mixture = openmc.Material.mix_materials(
